85b5107ffdf64522e6e0d31562dcdabbd983478a
Rafael Morais <rafael.acmorais@hotmail.com>
2025-03-22 20:18:08 -0300

    concluido

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        int conta;
+        float saldo;
+        String agencia;
+        String nomeCliente;
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta ");
+        conta = ler.nextInt();
+        ler.nextLine();
+
+        System.out.println("Por favor, digite o número da agência ");
+        agencia = ler.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente ");
+        nomeCliente = ler.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta ");
+        saldo = ler.nextFloat();
+
+        System.out.printf("Conta: %d Agência: %s Cliente: %s Saldo: %.2f \n", conta, agencia, nomeCliente, saldo);
+
+    }
+}
